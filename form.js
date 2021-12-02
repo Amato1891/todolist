75f4c6f4ff55f688fff092ea46e1d26928286c9d
Jim Amato <Amato1891@yahoo.com>
2021-12-02 16:48:43 -0500

    Added update feature/ some styling added

--- form.js
+++ form.js
@@ -1,23 +1,24 @@
 function TodoForm({addTodo}){
-    const [value, setValue] = React.useState('');
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-        if(!value) return;
-        addTodo(value);
-        setValue('');
-      }
-    
-    return( <>
-        <form onSubmit={handleSubmit}>
-        <input
-          type= 'text'
-          className='input'
-          value={value}
-          placeholder='Add More Things!...'
-          onChange={e => setValue(e.target.value)}
-          />
-      </form>
-      </>
-    );
+  const [value, setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+      e.preventDefault();
+      if(!value) return;
+      addTodo(value);
+      setValue('');
+    }
+  
+  return( <>
+      <form onSubmit={handleSubmit}>
+      <input
+        type= 'text'
+        className='input'
+        value={value}
+        placeholder='Add More Things!...'
+        onChange={e => setValue(e.target.value)}
+        />
+         <button onClick={handleSubmit}>Add todo</button>
+    </form>
+    </>
+  );
 }
--- todo.js
+++ todo.js
@@ -1,9 +1,20 @@
 function Todo({todo,index,remove}){
+    const [changeTodo, setChangeTodo] = React.useState('')
     function handle(){
         remove(index);
     }
-    return <> <div className="todo">{todo.text}
-     <button onClick={handle}>DELETE</button>
+
+    const handleUpdate = (e) => {
+        let newTodo = window.prompt('What would you like to change your todo to?...');
+        if (newTodo == '' || newTodo == ' ' ) return;
+        e.target.innerText = newTodo;
+        console.log(e.target.innerText)
+
+      }
+
+    return <> <div onClick={(e)=> {handleUpdate(e)}} id = {index}className="todo">{todo.text}
+     
      </div>
+     <button onClick={handle}>Remove</button>
      </>
 }
